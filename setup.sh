aa02e8404f998bbaa8fd5361241361ef0fe2bd89
Mustafa Erol <96944695+codeeleven0@users.noreply.github.com>
2026-02-22 16:39:38 +0300

    Add spin scripts (beta)

---
+++ setup.sh
@@ -0,0 +1,7 @@
+CNAME=fedora-pi-builder-$RANDOM
+docker rmi spin-pi-fedora-builder
+docker build -t spin-pi-fedora-builder .
+docker run -it --privileged=true --name $CNAME spin-pi-fedora-builder
+docker cp $CNAME:/root/artifacts/fedora-flash.img.xz .
+docker cp $CNAME:/root/artifacts/fedora-flash.img .
+docker rm $CNAME
