ba3a3c516cba8fd7359b59b0a3cf972cd2262e44
Mustafa Erol <96944695+codeeleven0@users.noreply.github.com>
2026-02-22 22:00:12 +0300

    remove img.xz build

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@ CNAME=fedora-pi-builder-$RANDOM
 docker rmi spin-pi-fedora-builder
 docker build -t spin-pi-fedora-builder .
 docker run -it --privileged=true --name $CNAME spin-pi-fedora-builder
-docker cp $CNAME:/root/artifacts/fedora-flash.img.xz .
 docker cp $CNAME:/root/artifacts/fedora-flash.img .
 docker rm $CNAME
 docker rmi spin-pi-fedora-builder
